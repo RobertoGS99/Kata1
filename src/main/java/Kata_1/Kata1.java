1eabc0b9ec7a1def73d2aa42b596d3c0227f9dbb
Admin <Admin@DESKTOP-UG0GOA7>
2021-09-24 14:04:33 +0100

    Actualizada la primera version de kata1: 	cambio date a calendar

--- src/main/java/Kata_1/Kata1.java
+++ src/main/java/Kata_1/Kata1.java
@@ -1,12 +1,15 @@
 package Kata_1;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Kata1 {
     
     public static void main(String[] args){
         System.out.println("hola mundo");
-        Person person = new Person ("Antonio", new Date(80,1,1));
+        Calendar birthdate = GregorianCalendar.getInstance();
+        birthdate.set(1980, 1, 1);
+        Person person = new Person ("Antonio", birthdate);
         System.out.println(person.getName()+" tiene " +person.getAge()+".");
     }
 }
--- src/main/java/Kata_1/Person.java
+++ src/main/java/Kata_1/Person.java
@@ -1,13 +1,14 @@
 package Kata_1;
 
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Person {
     private final String name;
-    private final Date birthdate;
+    private final Calendar birthdate;
     
-    public Person(String name, Date birthdate){
+    public Person(String name, Calendar birthdate){
         this.name = name;
         this.birthdate = birthdate;
     }
@@ -17,7 +18,13 @@ public class Person {
     }
 
     int getAge() {
-        return (int) ((new Date().getTime()-birthdate.getTime())/31536000000L);
+        Calendar now = GregorianCalendar.getInstance();
+        return millisecondsToYears(now.getTimeInMillis() - birthdate.getTimeInMillis());
+    }
+
+    private int millisecondsToYears(long l) {
+        long MillisecondsPerYear = (long) (365.25*24*60*60*1000);
+        return (int) (l/MillisecondsPerYear);
     }
     
     
